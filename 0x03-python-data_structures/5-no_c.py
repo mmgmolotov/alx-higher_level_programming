62758b725850f979be1e3a3dfb3fd3bac6894a89
mmgmolotov <mmgmolotov@gmail.com>
2023-09-11 17:37:49 -0700

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
 def no_c(my_string):
-    listofchars = list(my_string)
-    for char in listofchars:
-        if char == 'c' or char == 'C':
-            listofchars.remove(char)
-    return("".join(listofchars))
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
