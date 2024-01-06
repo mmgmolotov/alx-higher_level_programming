3de08d062febad638deadd81dbe334771008c3fb
mmgmolotov <mmgmolotov@gmail.com>
2024-01-06 03:51:57 +0100

    six

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-School-User-Id: 98" "${1}"
+# sends a GET request to the URL, and displays the body of the response
+curl -s -X GET -H "X-School-User-Id: 98" "$1"
