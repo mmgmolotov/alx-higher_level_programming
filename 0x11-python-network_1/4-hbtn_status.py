96ee9fa4621716965390411c3609c4971c19fe75
mmgmolotov <mmgmolotov@gmail.com>
2024-01-08 02:37:52 +0100

    fix2

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
-Script that fetches https://intranet.hbtn.io/status
+Script that fetches https://alx-intranet.hbtn.io/status
 """
+
 import requests
 
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    content_type = type(response.text)
+
     print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print("\t- type: {}".format(content_type))
+    print("\t- content: {}".format(response.text))
