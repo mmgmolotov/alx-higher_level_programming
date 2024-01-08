5eda39100c9cb4b671d91553c8354b03a4718fac
mmgmolotov <mmgmolotov@gmail.com>
2024-01-08 02:42:33 +0100

    fix3

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request as request
 
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         if response.readable():
             data = response.read()
             print("Body response:")
