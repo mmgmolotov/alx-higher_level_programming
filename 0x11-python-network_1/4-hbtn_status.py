24ef47ba53f21effc99d42239cbc9b303e5f8250
mmgmolotov <mmgmolotov@gmail.com>
2024-01-08 02:27:40 +0100

    fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Fetches a URL."""
+"""
+Script that fetches https://intranet.hbtn.io/status
+"""
 import requests
 
-
 if __name__ == '__main__':
-    response = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type: {}'.format(type(response.text)))
-    print('\t- content: {}'.format(response.text))
+    url = "https://intranet.hbtn.io/status"
+    r = requests.get(url)
+    text = r.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
+    
